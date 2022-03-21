d63a35820707f1611212bd0cd9208855d95bbaed
WillClarke <520379@chrome.escg.ac.uk>
2022-03-21 22:25:42 +0000

    enemy asset

---
+++ Assets/MainBuild/Scripts/ItemPickup.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b8f3f5368894bd6c8a1163cd4a9296d7440577ab8f6ab9ae3190af2edb5ae9f3
+size 277
--- Assets/Stealth/PlayerMovement.cs
+++ Assets/Stealth/PlayerMovement.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8481ff6c968dec3db6b689ceddb25fb5dcddea9b072348389d77a1d40f87dd1d
-size 1491
+oid sha256:14a7ec065fda4b1b973e9eaef900a98aa09b1cf95038944c05cd96aa8056f5a2
+size 1499

2e048c86237751fe1b0fa3f3e6327f0d9b4631db
WillClarke <520379@chrome.escg.ac.uk>
2022-03-23 22:23:25 +0000

    sounds, enemy anims, level building

--- Assets/Stealth/Guard.cs
+++ Assets/Stealth/Guard.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db766c3524ceb6a6de2d64621657853ee4c376e16822517c4232150b9de6c509
-size 4234
+oid sha256:352f4713a78d2624a1bef838738cfbfb12b3f180c46e1c5852b7c918dd53fad3
+size 4040
